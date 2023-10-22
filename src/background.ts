7f987d16913e6365902c53cd9dda2d59a4bd7598
Hiromu OCHIAI <otiai10@gmail.com>
2023-10-22 12:56:09 +0900

    Fix minimum

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,27 @@
 
-import browsericon from './routes/browsericon';
-chrome.action.onClicked.addListener(browsericon.listener());
+// import browsericon from './routes/browsericon';
+// chrome.action.onClicked.addListener(browsericon.listener());
 
-import alarm from './routes/alarm';
-chrome.alarms.onAlarm.addListener(alarm.listener());
+// import alarm from './routes/alarm';
+// chrome.alarms.onAlarm.addListener(alarm.listener());
+
+import { Router } from "chromite";
+
+const r = new Router<chrome.runtime.ExtensionMessageEvent>();
+
+r.on("/test", async function(m, s) {
+    console.log(m, s);
+    return "hoge";
+});
+r.onNotFound(async function(m, s) {
+    console.log("not found");
+    console.log(m, s);
+});
+
+console.log(r.listener());
+chrome.runtime.onMessage.addListener(r.listener());
+
+
+// chrome.runtime.onMessage.addListener((m, s, r) => {
+//     console.log(m, s, r);
+// });
